3e8d2c8c65ff9c1aa09465199ece49f629c01991
dary <darya.khalmatova0303@gmail.com>
2024-05-02 15:42:56 +0700

    Добавили функцию чтения из файла wrfile.txt

--- wrfile.c
+++ wrfile.c
@@ -3,7 +3,6 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-
 int main() {
         int fd;
         size_t size;
@@ -18,6 +17,21 @@ int main() {
                 exit(-1);
         }
         else printf("Строка записана в файл\n");
+        if (close(fd) < 0) {
+                printf("Не получилось закрыть файл\n");
+                exit(-1);
+        }
+        char resstring[14];
+        if((fd = open("wrfile.txt", O_RDONLY)) < 0) {
+                printf("Ошибка при открытии файла на чтение\n");
+                exit(-1);
+        }
+        size = read(fd, resstring, 14);
+        if (size != 14) {
+                printf("Не удалось прочитать 14 байт из файла\n");
+                exit(-1);
+        }
+        else printf("Прочитанная строка: %s\n", resstring);
         if (close(fd) < 0) {
                 printf("Не получилось закрыть файл\n");
                 exit(-1);
